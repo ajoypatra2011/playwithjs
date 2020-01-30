571b5ceca56c8f0870835a423b8add40fc52f11d
ajoypatra2011 <46737113+ajoypatra2011@users.noreply.github.com>
2020-01-30 19:51:07 +0530

    Add files via upload

---
+++ precache-manifest.974a129db04962d93b2319d8414aefe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b095373171ed36d2a71070451f69ccb2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ee765347bd7453baee2",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3951acf6cd7784882fd2",
+    "url": "./static/css/main.683a2f98.chunk.css"
+  },
+  {
+    "revision": "6ee765347bd7453baee2",
+    "url": "./static/js/2.63a0ab8f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "./static/js/2.63a0ab8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3951acf6cd7784882fd2",
+    "url": "./static/js/main.c3be98c2.chunk.js"
+  },
+  {
+    "revision": "924e3627391566a52d5c",
+    "url": "./static/js/runtime-main.b993e6a8.js"
+  }
+]);
